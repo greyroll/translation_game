3db048aa5533de38f78b0be78fd45c47203f62b8
lizzy <lizaveta.fiadotava@gmail.com>
2024-07-02 16:26:48 +0400

    02.07

--- tests.py
+++ tests.py
@@ -1,8 +1,2 @@
 from classes import Question
 
-
-# class TestQuestion:
-	# def test_check_answer_give_fb(self):
-	# 	question = Question(question="Beverage", answer="Напиток", options=['Меню', 'Заказ', 'Гарнир', 'Главное блюдо', 'Напиток'])
-	# 	assert question.check_answer_give_fb("Напиток") == True
-	# 	assert question.check_answer_give_fb("Меню") == False
